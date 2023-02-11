82e0c38a69da452ef123afee9033e31837a63a4c
Timerkhan <timerkhan12@gmail.com>
2023-02-11 17:37:01 +0500

    some usefull functions added

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива: ");
+int SIZE = Convert.ToInt32(Console.ReadLine());
+
+string[] NewArr (int SIZE)
+{
+    string[] arr = new string[SIZE];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = ((char)rand.Next('a', 'z' + 1)).ToString();
+    
+    }
+    return arr;
+}
+
+void ShowArr (string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+}
+
+string[] Arr = NewArr(SIZE);
+ShowArr(Arr);
